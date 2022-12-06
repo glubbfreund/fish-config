6e2fe46dbd59cadf5f6bb7c3c4c6f9636b313a2e
glubbfreund <oliver.leis@gmail.com>
2022-12-06 10:36:07 +0100

    moved status mode to the right side

--- functions/fish_mode_prompt.fish
+++ functions/fish_mode_prompt.fish
@@ -1,24 +1,3 @@
-function fish_mode_prompt
-    if [ $fish_key_bindings = fish_vi_key_bindings ]
-        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
-        switch $fish_bind_mode
-            case default
-                set_color 282A2E -b 72E6CC 
-                echo " NORMAL "
-                set_color normal
-                echo " "
-            case insert
-                echo ""
-            case visual
-                set_color 282A2E -b 9D7CD8 
-                echo " VISUAL "
-                set_color normal
-                echo " "
-            case replace_one
-                set_color 282A2E -b f7768e
-                echo " REPLACE "
-                set_color normal
-                echo " "
-        end
-    end
+function fish_default_mode_prompt
 end
+
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,5 +1,30 @@
 function fish_right_prompt
-  set -l last_status $status
+ if [ $fish_key_bindings = fish_vi_key_bindings ]
+    or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+    switch $fish_bind_mode
+        case default
+            set_color 282A2E -b 72E6CC 
+            echo " NORMAL "
+            set_color normal
+            echo " "
+        case insert
+            set_color 282A2E -b 9ECE6A 
+            echo " INSERT "
+            set_color normal
+            echo " "
+        case visual
+            set_color 282A2E -b 9D7CD8 
+            echo " VISUAL "
+            set_color normal
+            echo " "
+        case replace_one
+            set_color 282A2E -b f7768e
+            echo " REPLACE "
+            set_color normal
+            echo " "
+    end
+ end
+ set -l last_status $status
   if not test $last_status -eq 0
     set_color --bold 282A2E  -b D2042D
     echo -n ' ! '
